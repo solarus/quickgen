1ae83fd86947b60be2ca2a21d2df89d7213463ef
David Spångberg <solarus@tunna.org>
2013-12-07 11:25:45 +0100

    rewrite the testing using the new TH api

--- src/Testing/QuickGen.hs
+++ src/Testing/QuickGen.hs
@@ -80,28 +80,40 @@ randomMatching match = do
 
 genInt = 5 :: Int
 
-foo = $(constructors [| ( genInt
-                        , map
-                        , (+) :: Int -> Int -> Int
-                        , (*) :: Int -> Int -> Int
-                        , id
-                        , ($) :: (a -> b) -> a -> b
-                        )
-                      |])
+instances :: ClassEnv
+foo :: [(Exp, Type)]
+(instances, foo) = $(constructors [| ( genInt
+                                     , map
+                                     , (+)
+                                     , (*)
+                                     -- , return
+                                     -- , id
+                                     -- , ($) :: (a -> b) -> a -> b
+                                     )
+                                   |])
 
-t1 = (foo !! 2) ^. _2
+-- the type `Int -> Int -> Int'
+t1 = AppT (AppT ArrowT (ConT (mkName "Int"))) (AppT (AppT ArrowT (ConT (mkName "Int"))) (ConT (mkName "Int")))
 
 t1' = snd (extractPrimType t1)
 
-t2 = extractPrimType ((foo !! 5) ^. _2)
+t2 = ([] , [ VarT (mkName "b")
+           , VarT (mkName "a")
+           , AppT (AppT ArrowT (VarT (mkName "a"))) (VarT (mkName "b"))
+           ])
 
 c = listToContext 10 foo
 
 f n = let Just e = generate t1 n c in pprint e
 
-test = matchWith (head t1') t2
+g from to = maximumBy (comparing length) (map f [from..to])
 
 baz :: (forall a. a) -> (forall a. a)
 baz a = a
 
 id2 = id
+
+test = matchWith (head t1') t2
+
+bar :: Integral a => a -> a -> a
+bar a b = a + b
