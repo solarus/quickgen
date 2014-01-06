150e205602f1ccdd64e4c0680569ba173efe6e79
David Spångberg <solarus@tunna.org>
2014-01-06 18:36:05 +0100

    Refactor the TH module according to the API suggestion

--- src/Testing/QuickGen/TH.hs
+++ src/Testing/QuickGen/TH.hs
@@ -1,63 +1,61 @@
 {-# LANGUAGE TemplateHaskell #-}
 
 module Testing.QuickGen.TH
-       ( constructors
-       , printTH
+       ( defineLanguage
        ) where
 
-import Data.Functor ((<$>))
-import Data.List (nub, sort)
-import Data.Maybe (isJust)
-import Language.Haskell.TH.Syntax as TH
+import           Control.Monad ((>=>))
+import           Data.List (nub)
+import           Data.Maybe (isJust)
+import qualified Language.Haskell.TH as TH
+import           Language.Haskell.TH.Syntax (lift)
 
--- For debugging splices function
-printTH e = TH.runQ e >>= print
+import           Testing.QuickGen.THInstances ()
+import           Testing.QuickGen.Types as T
 
--- | Parses a list of constructors.
-constructors e = do
-    TH.TupE es <- e
-    (cs, es') <- unzip <$> mapM f es
-
-    rel <- getRelevantClasses (nub (concat cs))
-    rel' <- [| rel |]
-
-    return $ TupE [rel', ListE es'] --
+defineLanguage :: TH.ExpQ -> TH.ExpQ
+defineLanguage tup = do
+    TH.TupE es <- tup
+    defineLanguage' (map f es) >>= (lift >=> return)
   where
-    f e@(TH.VarE name) = do
-        info <- TH.reify name
+    f (TH.VarE name)             = (name, Nothing)
+    f (TH.SigE (TH.VarE name) t) = (name, Just t)
 
-        let t = case info of
-                TH.VarI _ t _ _     -> t
-                TH.ClassOpI _ t _ _ -> t
-
-        t' <- lift t
-        e' <- lift e
-        return (getClassnames t, (TH.TupE [e', t']))
-    f (TH.SigE e@(TH.VarE name) t) = do
-        e' <- lift e
-        t' <- lift t
-        return (getClassnames t, (TH.TupE [e', t']))
+defineLanguage' :: [(Name, Maybe TH.Type)] -> TH.Q Language
+defineLanguage' es = do
+    cs <- mapM f es
+    let ns = nub (concatMap (getClassNames . snd) cs)
+    env <- getClassEnv ns
+    return $ L env cs
+  where
+    f :: (Name, Maybe TH.Type) -> TH.Q Constructor
+    f (name, mt) = do
+        t <- case mt of
+            Nothing -> do
+                info <- TH.reify name
+                case info of
+                    TH.VarI     _ t' _ _ -> return t'
+                    TH.ClassOpI _ t' _ _ -> return t'
+            Just t' -> return t'
+        let t' = thTypeToType t
+        return (name, t')
 
 -- | Given a list of class names iteratively find new classes
 -- mentioned in either the constraints of a class name or in any of
--- the instances. Returns a list with information about all instances
--- for the initial classes and discovered classes.
-getRelevantClasses :: [TH.Name] -> TH.Q [(TH.Name, [TH.InstanceDec])]
-getRelevantClasses = go []
+-- the instances. Returns the `ClassEnv' with information about all
+-- instances for the initial classes and the discovered classes.
+getClassEnv :: [Name] -> TH.Q ClassEnv
+getClassEnv = go empty
   where
+    go :: ClassEnv -> [Name] -> TH.Q ClassEnv
     go acc [] = return acc
     go acc (n:ns) = do
-        d@(TH.ClassI (TH.ClassD cxt _ _ _ _) is) <- TH.reify n
-        let acc' = ((n, is) : acc)
-            new = nub $ [ n'
-                        | TH.ClassP n' _ <- cxt ++ concat [ cxt' | TH.InstanceD cxt' _ _ <- is ]
-                        , not (n' `elem` ns || isJust (lookup n' acc'))
-                        ]
+        TH.ClassI (TH.ClassD cxt _ _ _ _) is <- TH.reify n
+        let cxt' = thCxtToCxt cxt
+            acc' = insert n (getCxtNames cxt', is) acc
+            is'  = concatMap thCxtToCxt [ c | TH.InstanceD c _ _ <- is ]
+            new  = nub $ [ n'
+                         | n' <- getCxtNames (cxt' ++ is')
+                         , not (n' `elem` ns || isJust (T.lookup n' acc'))
+                         ]
         go acc' (new ++ ns)
-
--- | Extracts the class names mentioned in a constraint in a type.
--- Returns the empty list if the type is not of the form:
--- `Pred => Type'
-getClassnames :: TH.Type -> [TH.Name]
-getClassnames (TH.ForallT _ cxt _) = [ n | TH.ClassP n _ <- cxt ]
-getClassnames _                    = []
