e524d5b4184a2be8ad37533860cc3d213197bd7a
David Spångberg <david@tunna.org>
2014-02-14 16:11:15 +0100

    Update the common test file and add arbitrary instances for some Types

--- testing/Common.hs
+++ testing/Common.hs
@@ -1,16 +1,15 @@
+{-# OPTIONS -fno-warn-orphans #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+
 module Common
        ( module Data.Maybe
        , module Test.Framework
        , module Test.Framework.Providers.HUnit
+       , module Test.Framework.Providers.QuickCheck2
        , module Test.HUnit
        , module Testing.QuickGen.ExpGen
        , module Testing.QuickGen.TH
        , module Testing.QuickGen.Types
-
-
-       -- , assertBool
-       -- , assertFailure
-       -- , assertEqual
 
        , a0, a1, a2, a3, a4
        , e0, e1, e2, e3, e4
@@ -23,25 +22,30 @@ module Common
        , tup2
        ) where
 
+import Control.Applicative
+import Control.Arrow
+import Control.Monad
 import Data.Maybe
 import Test.Framework hiding (Seed)
 import Test.Framework.Providers.HUnit
+import Test.Framework.Providers.QuickCheck2
 import Test.HUnit (Assertion, assertBool, assertFailure, assertEqual)
+import Test.QuickCheck
 import Testing.QuickGen.ExpGen
 import Testing.QuickGen.TH
 import Testing.QuickGen.Types
 
 a0,a1,a2,a3,a4,e0,e1,e2,e3,e4 :: Variable
-a0 = (0, Forall)
-a1 = (1, Forall)
-a2 = (2, Forall)
-a3 = (3, Forall)
-a4 = (4, Forall)
-e0 = (10, Exists)
-e1 = (11, Exists)
-e2 = (12, Exists)
-e3 = (13, Exists)
-e4 = (14, Exists)
+a0 = (100, Forall)
+a1 = (101, Forall)
+a2 = (102, Forall)
+a3 = (103, Forall)
+a4 = (104, Forall)
+e0 = (110, Exists)
+e1 = (111, Exists)
+e2 = (112, Exists)
+e3 = (113, Exists)
+e4 = (114, Exists)
 
 tInt :: SType
 tInt = ConT (mkName "Int") []
@@ -51,13 +55,57 @@ a --> b = FunT [VarT b, VarT a]
 
 tMap :: Type
 tMap = Type [a, b] [] (FunT [ ListT (VarT b), ListT (VarT a), a --> b ])
-  where a = (100, Forall)
-        b = (101, Forall)
+  where a = (200, Forall)
+        b = (201, Forall)
 
 tConst :: Type
 tConst = Type [a, b] [] (FunT [ VarT a, VarT b, VarT a])
-  where a = (102, Forall)
-        b = (103, Forall)
+  where a = (202, Forall)
+        b = (203, Forall)
 
 tup2 :: Variable -> Variable -> Type
 tup2 v1 v2 = Type [v1,v2] [] (ConT (mkName "Tup2") [VarT v1, VarT v2])
+
+instance Arbitrary Quantifier where
+    arbitrary = elements [ Forall, Exists ]
+
+instance Arbitrary Name where
+    arbitrary = mkName <$> (getNonEmpty <$> arbitrary)
+
+newtype Var = Var { fromVar :: Variable }
+
+instance Arbitrary Var where
+    arbitrary = do
+        n <- getNonNegative <$> arbitrary
+        q <- arbitrary
+        return (Var (n, q))
+
+instance Arbitrary SType where
+    arbitrary = sized $ \n -> choose (0, min 1000 n) >>= go
+      where
+        go n = frequency [ (n, fun)
+                         , (5, var)
+                         , (n, con)
+                         , (n*2, list)
+                         ]
+          where
+            n' = n `div` 2
+            var = VarT . fromVar <$> arbitrary
+            fun = do
+                k <- choose (2, 4)
+                FunT <$> replicateM k (go n')
+            con = do
+                (k, name) <- elements . map (second mkName) $
+                               [ (0, "Int")
+                               , (0, "Double")
+                               , (2, "Tup2")
+                               , (3, "Tup3")
+                               , (1, "Maybe")
+                               ]
+                ConT name <$> replicateM k (go n')
+            list = ListT <$> go n'
+
+instance Arbitrary Type where
+    arbitrary = do
+        st <- arbitrary
+        return $ Type (getVars st) [] st
