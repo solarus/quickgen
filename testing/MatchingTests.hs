e753854be53e746d914b4e206b6f17c3bbcddb55
David Spångberg <david@tunna.org>
2014-02-14 16:08:42 +0100

    update MatchingTests

--- testing/MatchingTests.hs
+++ testing/MatchingTests.hs
@@ -10,12 +10,12 @@ testAll :: Test
 testAll = testGroup "Matching tests "
     [ testCase "match1" $ do
            let tup@(Type _ _ stup) = bindForall $ tup2 a0 e0
-               e103 = (103, Exists)
-               expected = Just ( Type [a0, e0, e103] [] (FunT [ stup, VarT e103, stup ])
+               expected = Just ( Type [e0, (203, Exists)] []
+                                      (FunT [ stup, VarT (203, Exists), stup ])
                                , emptySubst)
            simpleMatch tup tConst expected
     ]
 
 simpleMatch t1 t2 expected = do
     let res = runStateT (match t1 t2) emptySubst
-    assertEqual "" res expected
+    assertEqual "" expected res
