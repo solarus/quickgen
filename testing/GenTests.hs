3d924a520e56f63208b72835d8e07f5ac219cc58
David Spångberg <david@tunna.org>
2014-03-31 18:33:43 +0200

    Add polymorphic function generation test case

--- testing/GenTests.hs
+++ testing/GenTests.hs
@@ -6,7 +6,6 @@ module GenTests
 
 import Common
 import Control.Exception.Base
-import Data.Dynamic
 import GHC hiding (Type)
 import GHC.Paths (libdir)
 import System.Random
@@ -15,35 +14,36 @@ import Language.Simple
 
 testAll :: Seed -> Test
 testAll s = testGroup "Generation tests "
-    [ testCase "gen1" $ testGen s (Type [] [] (ListT tInt))
+    [ testCase "generate expressions of type :: a -> [a]" $
+        testGen s (Type [a] [] (FunT [ListT (VarT a), VarT a])) "a -> [a]"
+    , testCase "generate expressions of type :: [Int]" $
+        testGen s (Type [] [] (ListT tInt)) "[Int]"
     ]
+  where
+    a = (0, Forall)
 
-testGen :: Seed -> Type -> Assertion
-testGen s t = let gen = mkStdGen s
-                  ss  = take 100 (randomRs (0,10000) gen)
-              in mapM_ p ss
+testGen :: Seed -> Type -> String -> Assertion
+testGen s t mt = let gen = mkStdGen s
+                     ss  = take 50 (randomRs (0,1000000) gen)
+                 in mapM_ p ss
   where
     p s' = do
         let (g, _) = generate lang t s'
         putStrLn (show g)
         case g of
             Nothing -> return ()
-            Just g  -> do
+            Just g' -> do
                 let exprStr = unwords [ "let genInt = 0 :: Int;"
                                       , "genDouble = 1 :: Double;"
                                       , "nil = [];"
                                       , "cons = (:)"
-                                      , "in (" ++ show g ++ ")"
-                                      , "`asTypeOf` (undefined :: [Int])"
+                                      , "in (" ++ show g' ++ ") :: " ++ mt
                                       ]
-
-                    go = do
-                        r <- runGhc (Just libdir) $ do
-                            _ <- getSessionDynFlags >>= setSessionDynFlags
-                            setContext [ IIDecl . simpleImportDecl . mkModuleName $ "Prelude" ]
-                            dynCompileExpr exprStr
-                        let expr = fromDyn r (error "wat" :: [Int])
-                        return (Right expr)
+                    go = runGhc (Just libdir) $ do
+                        _ <- getSessionDynFlags >>= setSessionDynFlags
+                        setContext [ IIDecl . simpleImportDecl . mkModuleName $ "Prelude" ]
+                        r <- compileExpr exprStr
+                        r `seq` return (Right r)
 
                 res <- catch go $ \e -> return (Left (e :: SomeException))
                 case res of
